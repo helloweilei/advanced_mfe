0fa13830cb0dd94589f1144d4ffccfbecf7f2f03
Wei Lei <lei.wei@thoughtworks.com>
2023-07-03 22:08:42 +0800

    init container

---
+++ marketing/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StylesProvider } from '@material-ui/core/styles';
+import { Switch, Route, BrowserRouter  } from 'react-router-dom';
+import Pricing from './components/Pricing';
+import Landing from './components/Landing';
+
+export default () => {
+  return <div>
+    <StylesProvider>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/pricing' component={Pricing}></Route>
+          <Route path='/' component={Landing}></Route>
+        </Switch>
+      </BrowserRouter>
+    </StylesProvider>
+  </div>;
+}
